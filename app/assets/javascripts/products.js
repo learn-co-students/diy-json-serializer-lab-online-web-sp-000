756423a151ccb529e282213f10ce09d8811c86a5
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-24 12:18:46 -0500

    comment out buggy tests

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,5 +1,11 @@
-console.log("hello from products.js");
-
-function showNext() {
- debugger 
+function showNext(currentId) {
+ let nextId = currentId + 1 
+//  debugger
+ $.getJSON(`/products/${nextId}/data`, (data) => {
+   console.log(data);
+   $('.name').text(data['name'])
+   $('.price').text(data['price'])
+   $('.inventory').text(data['inventory'])
+   $('.description').text(data['description'])
+ })
 }
